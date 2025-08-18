b34581195e3cfdef1a01522980b3d683fa3c7106
Zagaz <andre_ranulfo@hotmail.com>
2025-08-18 12:27:49 -0300

    Refactor ACF listing card layout; adjust image rendering logic and update flex alignment for better styling

--- includes/acf-listing-card.php
+++ includes/acf-listing-card.php
@@ -2,14 +2,7 @@
 // This is the vcards 
 ?>
 <div class="<?php echo esc_attr($blockClass . '-grid-item'); ?>">
-    <?php if (has_post_thumbnail()) : ?>
-        <img src="<?php echo esc_url(get_the_post_thumbnail_url()); ?>" alt="<?php the_title(); ?>" class="<?php echo esc_attr($blockClass . '-grid-item-image'); ?>" />
-    <?php else : ?>
-        <!-- Use lorem picsum random image -->
-        <?php $random_id = rand(1, 1000); ?>
-        <img src="<?php echo esc_url('https://picsum.photos/480/360?random=' . $random_id); ?>" alt="<?php the_title(); ?>" class="<?php echo esc_attr($blockClass . '-grid-item-image'); ?>" />
-    <?php endif; ?>
-    <?php 
+      <?php 
          $term_tags = "";
         if (count($term_name) > 1) {
             $term_tags = '<i class="fa-solid fa-tags"></i>';
@@ -27,6 +20,14 @@
         <?php endif; ?>
 
     </div>
+    <?php if (has_post_thumbnail()) : ?>
+        <img src="<?php echo esc_url(get_the_post_thumbnail_url()); ?>" alt="<?php the_title(); ?>" class="<?php echo esc_attr($blockClass . '-grid-item-image'); ?>" />
+    <?php else : ?>
+        <!-- Use lorem picsum random image -->
+        <?php $random_id = rand(1, 1000); ?>
+        <img src="<?php echo esc_url('https://picsum.photos/480/360?random=' . $random_id); ?>" alt="<?php the_title(); ?>" class="<?php echo esc_attr($blockClass . '-grid-item-image'); ?>" />
+    <?php endif; ?>
+  
 
     <h2 class="<?php echo esc_attr($blockClass . '-grid-item-title'); ?>"><?php the_title(); ?></h2>
     <div class="<?php echo esc_attr($blockClass . '-grid-item-excerpt'); ?>"><?php the_excerpt(); ?></div>
